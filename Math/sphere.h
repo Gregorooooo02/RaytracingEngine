4cb90e497a8b3aa2c911ae4df319211d5659c9b7
heshsaih <heshsaih@gmail.com>
2025-03-21 18:59:04 +0100

    feat: complete shpere hit implementation, things n such :-)

--- Math/sphere.h
+++ Math/sphere.h
@@ -4,7 +4,7 @@
 #include "vec3.h"
 
 namespace math {
-  class sphere {
+  class sphere : public primitive {
     public:
       vec3 center;
       float radius;
@@ -12,7 +12,7 @@ namespace math {
       sphere(vec3 &center,  float radius);
       sphere(const sphere &sphere);
       ~sphere();
-      bool hit(ray &ray, float tMin, float tMax);
+      bool hit(ray &ray, float tMin, float tMax) override;
   };
 
 }
