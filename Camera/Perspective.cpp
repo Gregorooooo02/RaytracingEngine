0f79802360f920678251a4e721c1d4fb7aea66d4
Grzegorz Bednarek <gregorooooo02@gmail.com>
2025-03-27 22:42:16 +0100

    Still trying to figure out whats going on; will leave it here for now

--- Camera/Perspective.cpp
+++ Camera/Perspective.cpp
@@ -8,25 +8,38 @@ Perspective::Perspective(const math::vec3 &pos, const math::vec3 &tgt, const mat
     : Camera(pos, tgt, upVec, nearP, farP), fov(fov) {}
 
 math::ray Perspective::generateRay(int pixelX, int pixelY, int imgWidth, int imgHeight) {
-    float a = (pixelX + 0.5f) / imgWidth;
-    float b = (pixelY + 0.5f) / imgHeight;
+    math::vec3 u, v, w;
+    math::vec3 origin = position;
 
-    math::vec3 w = (position - target).normalize();
-    math::vec3 u = up.crossProduct(w).normalize();
-    math::vec3 v = w.crossProduct(u);
-
-    float aspectRatio = static_cast<float>(imgWidth) / static_cast<float>(imgHeight);
+    float focalLength = (position - target).len();
     float theta = fov * M_PI / 180.0f;
+    float aspectRatio = float(imgWidth) / float(imgHeight);
 
-    float halfHeight = tan(theta/2.0f);
+    float halfHeight = 2.0f * tan(theta / 2.0f) * focalLength;
     float halfWidth = aspectRatio * halfHeight;
 
-    math::vec3 lower_left_corner = position - halfWidth * u - halfHeight * v - w;
-    math::vec3 horizontal = 2.0f * halfWidth * u;
-    math::vec3 vertical = 2.0f * halfHeight * v;
+    w = (position - target).normalize();
+    u = up.crossProduct(w).normalize();
+    v = w.crossProduct(u);
 
-    math::vec3 pointOnPlane = lower_left_corner + a * horizontal + b * vertical;
-    math::vec3 direction = (pointOnPlane - position).normalize();
+    math::vec3 horizontal = halfWidth * u;
+    math::vec3 vertical = halfHeight * v;
 
-    return {position, direction};
+    math::vec3 pixelDeltaU = horizontal / float(imgWidth);
+    math::vec3 pixelDeltaV = vertical / float(imgHeight);
+
+    math::vec3 upperLeft = origin - (w * focalLength) - horizontal/2 - vertical/2;
+
+    float s = float(pixelX) / float(imgWidth);
+    float t = float(pixelY) / float(imgHeight);
+
+    math::vec3 pixelPos = upperLeft + s * horizontal + t * vertical;
+    math::vec3 direction = pixelPos - origin;
+
+    math::ray ray(
+        origin,
+        direction
+    );
+
+    return ray;
 }
--- main.cpp
+++ main.cpp
@@ -13,26 +13,27 @@ int main() {
     int img_height = 800;
 
     cam::Perspective camera(
-        math::vec3(0, 0, 1),        // Camera position
-        math::vec3(0, 0, 0),        // Target position
+        math::vec3(0, 0, 0),        // Camera position
+        math::vec3(0, 0, -1),        // Target position
         math::vec3(0, 1, 0),        // Up vector
         0.1f,                             // Near plane
-        100.0f,                           // Far plane
-        110.0f                             // Field of view
+        1000.0f,                           // Far plane
+        90.0f                             // Field of view
     );
 
     cam::LightIntensity sphere_color1(1.0, 0.0, 0.0);
     cam::LightIntensity background_color(0.0, 0.0, 0.0);
 
-    math::vec3 sphere_center(0, 0, 0);
-    math::sphere sphere(sphere_center, .8f);
+    float R = M_PI / 4;
+    math::vec3 sphere_center(0, 0, -2);
+    math::sphere sphere(sphere_center, R);
 
     cam::Image img(img_width, img_height);
 
     for (int y = 0; y < img_height; y++) {
         for (int x = 0; x < img_width; x++) {
             math::ray ray = camera.generateRay(x, y, img_width, img_height);
-            if (sphere.hit(ray, 0.0f, 100.0f)) {
+            if (sphere.hit(ray, 0.0f, 1000.0f)) {
                 img.setPixel(x, y, sphere_color1);
             } else {
                 img.setPixel(x, y, background_color);
