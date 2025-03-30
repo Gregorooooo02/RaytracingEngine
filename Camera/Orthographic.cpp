9e6433fad4a542ef5d60ad178fc07d569d55c475
Grzegorz Bednarek <gregorooooo02@gmail.com>
2025-03-30 09:32:20 +0200

    Antialiasing works on orthographic camera

--- Camera/Orthographic.cpp
+++ Camera/Orthographic.cpp
@@ -9,11 +9,18 @@ math::ray Orthographic::generateRay(int pixelX, int pixelY, int imgWidth, int im
     float pixelWidth = 2.0f  / imgHeight;
     float pixelHeight = 2.0f / imgWidth;
 
-    float x = -1.0f + (pixelX + 0.5f) * pixelWidth;
-    float y = 1.0f - (pixelY + 0.5f) * pixelHeight;
+    auto offset = sampleSquare();
+
+    float x = -1.0f + (pixelX + offset.x) * pixelWidth;
+    float y = 1.0f - (pixelY + offset.y) * pixelHeight;
 
     math::vec3 origin = position + math::vec3(x, y, 0.0f);
     math::vec3 dir = (target - position).normalize();
 
-    return {origin, dir};
+    math::ray ray(
+        origin,
+        dir
+    );
+
+    return ray;
 }
